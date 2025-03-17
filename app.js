b9fe1c1d60b8754c01fcc90cb7d890825192c29e
gekishou <gonfuentes112@gmail.com>
2025-03-17 20:03:57 -0300

    add ejs support

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 require("dotenv").config();
 const express = require("express");
+const path = require('path');
+
 const typeRouter = require("./routes/typeRouter");
 const trainerRouter = require("./routes/trainerRouter");
 const pokemonRouter = require("./routes/pokemonRouter");
+
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.use("/types", typeRouter);
 app.use("/trainers", trainerRouter);

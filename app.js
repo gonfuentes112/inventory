05066083b21c56203f421998eac0953d2292261b
gekishou <gonfuentes112@gmail.com>
2025-03-17 19:42:19 -0300

    add basic app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const express = require("express");
+const typeRouter = require("./routes/typeRouter");
+const trainerRouter = require("./routes/trainerRouter");
+const pokeomnRouter = require("./routes/pokemonRouter");
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/types", typeRouter);
+app.use("/trainers", trainerRouter);
+app.use("/pokemons", pokemonRouter);
+
+app.get("/", async (req, res) => {
+  res.send("home");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, console.log(`listening on http://localhost:${PORT}`));

7da3f594a7f6332757bcae23b76ed59192b62f5a
gekishou <gonfuentes112@gmail.com>
2025-03-17 19:15:11 -0300

    add trainerrouter

---
+++ routes/trainerRouter.js
@@ -0,0 +1,12 @@
+const { Router } = require("express");
+const trainersRouter = Router();
+const trainersController = require("../controller/trainersController");
+
+trainersRouter.get("/:id", trainersController.getTrainerById);
+trainersRouter
+  .get("/", trainersController.getTrainers)
+  .post("/", trainersController.postTrainer)
+  .patch("/", trainersController.updateTrainer)
+  .delete("/", trainersController.deleteTrainer);
+
+module.exports = trainersRouter;
